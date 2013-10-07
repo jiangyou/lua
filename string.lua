be90d4a7b12c539cdf6032b49b922ab58ba3aeb2
jiangyou <1253300707@qq.com>
2013-10-07 08:46:53 +0800

    project configuration file

--- string.lua
+++ string.lua
@@ -1,7 +1,11 @@
 local function test1()
-  date = "17/3/1990";
-    _,_,d,m,y = string.find(date,"(%d+)/(%d+)/(%d+)");
-    print(d,m,y);
+--  date = "17/3/1990";
+--    _,_,d,m,y = string.find(date,"(%d+)/(%d+)/(%d+)");
+--    print(d,m,y);
+    local s = [[then he said: "it's all rigth"!]];
+    a,b,c,part = string.find(s,"([\"\'])(.-)%1");
+    print(part);
+    print(c)
 end
 
 
